37a674c433d1fc051c3b0a51efe1b29a7c5d7edd
user1 <plora.ayvazyan053@hum.tsu.edu.ge>
2024-11-02 01:30:36 +0400

    exceptionsStringOperatins

--- src/main/java/ge/tbc/testautomation/javaoop/figures/Figure.java
+++ src/main/java/ge/tbc/testautomation/javaoop/figures/Figure.java
@@ -1,7 +1,17 @@
 package ge.tbc.testautomation.javaoop.figures;
 //figures პაკეტში შექმენით კლასი Figure და იქ გაწერეთ
 // ორი ღია უპარამეტრო მეთოდი getArea და getPerimeter. ორივე აბრუნებს ათწილადს, -1.0-ს.
+
+//// Figure კლასს გაუწერეთ ერთი public static int ველი numberOfInstances
+//// და ამავე კლასის უპარამეტრო კონსტრუქტორში გაზარდეთ იგი, ან დატოვეთ იგივე წინა დავალებიდან.\
+
 public class Figure {
+    public static int numberOfInstances;
+
+    public Figure() {
+        numberOfInstances++;
+    }
+
     public double getArea(){
         return -1.0;
     }
