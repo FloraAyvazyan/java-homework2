954d99c1ff9c6bdf2fcc6c239b0bb2669e82a7d2
user1 <plora.ayvazyan053@hum.tsu.edu.ge>
2024-11-01 22:24:58 +0400

    exceptionsStringOperations branch

--- src/main/java/ge/tbc/testautomation/javaoop/figures/Circle.java
+++ src/main/java/ge/tbc/testautomation/javaoop/figures/Circle.java
@@ -1,23 +1,35 @@
 package ge.tbc.testautomation.javaoop.figures;
 
-public class Circle {
+
+
+//Circle-ს უნდა ჰქონდეს მხოლოდ ერთი private double ველი radius.
+// ამ კლასს გაუწერეთ სეთერები/გეთერები და ერთ პარამეტრიანი (რადიუსი) კონსტრუქტორი,
+
+import ge.tbc.testautomation.javaoop.exceptionsStringOperationsRegex.LimitException;
+import ge.tbc.testautomation.javaoop.exceptionsStringOperationsRegex.RadiusException;
+
+//სადაც შეამოწმეთ, მეტია თუ არა numberOfInstances 5-ზე. თუ არის -
+// ისროლეთ LimitException მესიჯით "INSTANTIATION LIMIT REACHED".
+//Circle-ს კონსტრუქტორშივე შეამოწმეთ რადიუსი არის თუ არა უარყოფითი მნიშვნელობა.
+// თუ არის - ისროლეთ RadiusException მესიჯით "RADIUS VALUE NOT VALID".
+public class Circle extends Figure {
     private double radius;
-    public static int numberOfCircleInstances;
 
-    //Getter ფუნქცია
-    public double getRadius() {
-        return radius;
-    }
-
-    //Setter ფუნქცია
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
-    //პარამეტრიანი კონსტრუქტორი
-    public Circle(double radius) {
-        this.radius = radius;
-        numberOfCircleInstances++;
+    public double getRadius() {
+        return radius;
     }
 
+    public Circle(double radius) {
+        if(numberOfInstances > 5){
+            throw new LimitException("INSTANTIATION LIMIT REACHED");
+        }
+        if(radius <0){
+            throw new RadiusException("RADIUS VALUE NOT VALID");
+        }
+        this.radius = radius;
+    }
 }
